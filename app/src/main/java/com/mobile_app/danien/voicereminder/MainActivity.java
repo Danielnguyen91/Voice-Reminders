2344ceec958cf2e0bdfa637cbcfa859382cb42b5
Danielnguyen91 <ctoan0130@gmail.com>
2016-03-18 00:11:58 -0700

    add reminder activity

--- app/src/main/java/com/mobile_app/danien/voicereminder/MainActivity.java
+++ app/src/main/java/com/mobile_app/danien/voicereminder/MainActivity.java
@@ -1,5 +1,7 @@
 package com.mobile_app.danien.voicereminder;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -11,14 +13,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView backgroundImg = (ImageView) findViewById(R.id.plus_Image);
-        backgroundImg.setBackgroundColor(Color.rgb(100, 100, 50));
+        ImageView Img = (ImageView) findViewById(R.id.plus_Image);
+        Img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ReminderActivity.class);
+                startActivity(intent);
+
+            }
+        });
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
